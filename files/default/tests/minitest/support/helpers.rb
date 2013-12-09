cde9403a1f1a1587aaefd6f9a4b097549586cc6e
Brian Flad <bflad417@gmail.com>
2013-12-08 22:25:04 -0500

    Rubocop fixes

--- files/default/tests/minitest/support/helpers.rb
+++ files/default/tests/minitest/support/helpers.rb
@@ -1,8 +1,9 @@
+# Helpers module
 module Helpers
+  # Helpers::Oculus module
   module Oculus
     include MiniTest::Chef::Assertions
     include MiniTest::Chef::Context
     include MiniTest::Chef::Resources
-
   end
 end
