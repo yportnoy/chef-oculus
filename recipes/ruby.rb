74ef5e0e2a06b75143e0890b6abfc50e421d9322
Yasha Portnoy <yportnoy@optoro.com>
2015-05-06 01:05:21 -0400

    Rubygems already included

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,6 +1,6 @@
 packages = value_for_platform_family(
-  'debian' => %w(ruby ruby-dev rubygems),
-  %w(fedora rhel) => %w(ruby ruby-devel rubygems)
+  'debian' => %w(ruby ruby-dev),
+  %w(fedora rhel) => %w(ruby ruby-devel)
 )
 
 packages.each do |p|
